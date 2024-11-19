1e9c4ac491fcf6615f4dee7161d55cc4493503e3
Hayden Howell <hayde@Haydens-MacBook-Air.local>
2024-11-19 14:21:56 -0500

    uip

--- public/script.js
+++ public/script.js
@@ -1,21 +1,28 @@
-const dateElement = document.getElementById('date');
-const currentDate = new Date().toDateString();
-dateElement.textContent = currentDate;
-
-// JavaScript to update workout information daily
-// You can modify this part to add different workouts each day
-document.getElementById('warmup-exercise1').textContent = '10 min light jog';
-document.getElementById('warmup-exercise2').textContent = 'Dynamic stretches';
+const url = "http://127.0.0.1:5000/data";
 
 
-document.getElementById('olympic-description').textContent = 'EMOM for 10 Minutes:';
-document.getElementById('olympic-exercise1').textContent = '3 Hang Cleans @75% of 1 rep max';
-document.getElementById('olympic-exercise2').textContent = '5 Toes to Rings';
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json(); // Convert the response to a JavaScript object
+  })
+  .then(data => {
+    // Use the JSON data here
 
-document.getElementById('wod-description').textContent = '5 Rounds for Time:';
-document.getElementById('wod-exercise1').textContent = '- 15 Air Squats';
-document.getElementById('wod-exercise2').textContent = '- 10 Push-Ups';
-document.getElementById('wod-exercise3').textContent = '- 5 Burpees';
 
-document.getElementById('cooldown-exercise1').textContent = '5 min walk';
-document.getElementById('cooldown-exercise2').textContent = 'Static stretches';
+
+    for (const value of data[0].wod_movements) {
+      console.log(value); // This should print each string in the array
+    }
+
+
+
+
+
+  })
+  .catch(error => {
+    // Handle any errors that occur
+    console.error('There has been a problem with your fetch operation:', error);
+  });
